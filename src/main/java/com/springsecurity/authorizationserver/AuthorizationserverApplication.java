41b8d2084d8776a8d8041b01b63826cc72cf9ea8
Monika <monikashekhawat553@gmail.com>
2019-09-01 20:49:59 +0530

    first commit

---
+++ src/main/java/com/springsecurity/authorizationserver/AuthorizationserverApplication.java
@@ -0,0 +1,18 @@
+package com.springsecurity.authorizationserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@SpringBootApplication
+@EnableAuthorizationServer
+class AuthorizationServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuthorizationServerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/springsecurity/authorizationserver/repository/UserDetailRepository.java
@@ -0,0 +1,14 @@
+package com.springsecurity.authorizationserver.repository;
+
+import com.springsecurity.authorizationserver.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.jws.soap.SOAPBinding;
+import java.util.Optional;
+
+public interface UserDetailRepository extends JpaRepository<User,Integer> {
+
+
+    Optional<User> findByUsername(String name);
+
+}
